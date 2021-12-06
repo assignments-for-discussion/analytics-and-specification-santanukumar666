bc5b1664585b26154ac524b9ba5533ca723f1be5
SANTANU KUMAR <60546202+santanukumar666@users.noreply.github.com>
2021-12-06 18:23:04 +0530

    corrected average.js

--- average.js
+++ average.js
@@ -1,6 +1,8 @@
 
 function average(numbers) {
   return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  const numbers = numbers.filter(n => !Number.isNaN(n));
+  return numbers.reduce((p, c) => p + c, 0) / numbers.length;
 }
 
 module.exports = {average};
